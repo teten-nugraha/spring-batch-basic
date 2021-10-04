31e63f107ed82091eddd061e169213eaf4372301
Teten Nugraha <teten.nugraha18@gmail.com>
2021-10-04 10:54:10 +0700

    first project

---
+++ src/main/java/id/ten/springbatchbasic/SpringBatchBasicApplication.java
@@ -0,0 +1,13 @@
+package id.ten.springbatchbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchBasicApplication.class, args);
+	}
+
+}
